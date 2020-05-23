095f34f92a915eeb53393087fd62872440312b11
mea <3232661683@qq.com>
2020-05-23 16:02:50 +0800

    登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {Input, Message, FormItem, Form, Button} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+Vue.prototype.$message = Message
