0e2027438c6c6cf1af837c813a474fe1e44771f2
mea <3232661683@qq.com>
2020-06-02 21:11:35 +0800

    完成表报开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -28,6 +29,7 @@ const router = new VueRouter({
       {path:'/goods', component:List},
       {path:'/orders', component:Order},
       {path:'/goods/add', component:Add},
+      {path:'/reports', component:Report},
 
     ]}
   ]
